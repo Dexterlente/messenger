fabff82cb6b2a5142a7044200773d47248484572
dexter <dexter.lente18@gmail.com>
2025-05-27 20:30:28 +0800

    update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,6 @@ import { Platform } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 
-import { HelloWave } from 'components/HelloWave';
 import { ThemedText } from 'components/ThemedText';
 import { ThemedView } from 'components/ThemedView';
 
@@ -12,7 +11,6 @@ export default function HomeScreen() {
      <SafeAreaView style={{ flex: 1, paddingHorizontal: 16,backgroundColor: '#ffffff' }}>
       <ThemedView className="flex-row items-center pt-5 space-x-2 pt-5">
         <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
 
       </ThemedView>
 
